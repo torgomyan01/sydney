2f25e111be04ce2ba666da75fdb57f954e592ac1
armanpinachyan <pinachyan99@mail.ru>
2023-09-14 19:31:11 +0400

    19:31

--- js/script.js
+++ js/script.js
@@ -77,6 +77,7 @@ window.addEventListener('scroll', (e)=>{
         }
 })
 
+//..... Gaps
 
 // if(window.screenX <= 576){
 //     gsap.registerPlugin(ScrollTrigger);
@@ -92,3 +93,73 @@ window.addEventListener('scroll', (e)=>{
 //     });
 //
 // }
+
+
+if(window.screenX <= 576){
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".content .box", {
+        x: -400,
+        scrollTrigger: {
+            trigger: '.content .box',
+            scrub: true,
+            end: '-200%',
+            toggleActions: 'restart none none none',
+        },
+        duration: 1
+    });
+
+}
+
+
+const sliders = document.getElementById('slider');
+const sliders2 = document.getElementById('slider2');
+const sliders3 = document.getElementById('slider3');
+
+noUiSlider.create(sliders2, {
+    start: [38700000, 98700000],
+    connect: true,
+    format: wNumb({
+        prefix: '$ ',
+        decimals: 0,
+        thousand: ','
+    }),
+    tooltips: true,
+    range: {
+        'min': 28700000,
+        'max': 110000000
+    }
+});
+
+noUiSlider.create(sliders, {
+    start: [5, 15],
+    connect: true,
+    tooltips: true,
+    format: wNumb({
+        decimals: 0,
+    }),
+    range: {
+        'min': 1,
+        'max': 30
+    }
+});
+
+
+noUiSlider.create(sliders3, {
+    start: [5000000, 9000000],
+    connect: true,
+    tooltips: true,
+    format: wNumb({
+        prefix: '$ ',
+        decimals: 0,
+        thousand: ','
+    }),
+    range: {
+        'min': 5000000,
+        'max': 13000000
+    }
+});
+
+
+//........ Input Range
+//
