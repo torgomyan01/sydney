0404bd3763529908d3fa17dac75e0ebcf61123f7
Andranik Torgomyan <andranik@nativeteams.com>
2023-09-16 00:29:42 +0300

    start animation

--- js/script.js
+++ js/script.js
@@ -332,3 +332,25 @@ if(!fixButtonStorage){
 
 
 
+// GSAP NUMBERS
+
+const items = document.querySelectorAll(".number-animation");
+
+gsap.from(items, {
+    textContent: 0,
+    duration: 2,
+    ease: "power1.in",
+    snap: { textContent: 1 },
+    stagger: {
+        each: 1.0,
+        onUpdate: function() {
+            this.targets()[0].innerHTML = numberWithCommas(Math.ceil(this.targets()[0].textContent));
+        },
+    }
+});
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
